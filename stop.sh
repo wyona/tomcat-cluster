1f4072f6339e93673eed98c4c3db6da16f93bca7
imboden <imboden@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-02-03 09:47:53 +0000

    integrated the balancer servlet into the cluster-framework

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./build/apache/bin/apachectl stop
-
+./build/jakarta-tomcat-5.0.30-balancer/bin/shutdown.sh
 ./build/jakarta-tomcat-5.0.30-1/bin/shutdown.sh
 ./build/jakarta-tomcat-5.0.30-2/bin/shutdown.sh
