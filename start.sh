efc22aae0f4259cac0a69c4fb164b4dc8e01b006
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-02-05 00:08:24 +0000

    node names changed

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 ./build/apache/bin/apachectl start
 ./build/jakarta-tomcat-5.0.30-balancer/bin/startup.sh
-./build/jakarta-tomcat-5.0.30-1/bin/startup.sh
-./build/jakarta-tomcat-5.0.30-2/bin/startup.sh
+./build/jakarta-tomcat-5.0.30-cnode1/bin/startup.sh
+./build/jakarta-tomcat-5.0.30-cnode2/bin/startup.sh
