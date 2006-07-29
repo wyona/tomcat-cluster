4606cb999d05f85cc345bd3e1a40745474466c27
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-29 10:53:28 +0000

    check if file exists

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-./build/apache/bin/apachectl start
+if [ -f build/apache/bin/apachectl ]; then
+  ./build/apache/bin/apachectl start
+fi
 ./build/jakarta-tomcat-5.0.30-balancer/bin/startup.sh
 ./build/jakarta-tomcat-5.0.30-cnode1/bin/startup.sh
 ./build/jakarta-tomcat-5.0.30-cnode2/bin/startup.sh
