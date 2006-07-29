effef3b6371136b512a223e8f17d71fc73a90993
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-07-29 10:54:47 +0000

    check if file exists

--- stop.sh
+++ stop.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-./build/apache/bin/apachectl stop
+if [ -f build/apache/bin/apachectl ]; then
+  ./build/apache/bin/apachectl stop
+fi
 ./build/jakarta-tomcat-5.0.30-balancer/bin/shutdown.sh
 ./build/jakarta-tomcat-5.0.30-cnode1/bin/shutdown.sh
 ./build/jakarta-tomcat-5.0.30-cnode2/bin/shutdown.sh
